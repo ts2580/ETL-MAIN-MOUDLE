f40e3b786c85bd6027a4b9a7204d1227a4a33919
ykkim <lemonsyain12@gmail.com>
2024-03-05 16:41:09 +0900

    변경

--- src/main/java/com/etl/sfdc/config/model/dto/User.java
+++ src/main/java/com/etl/sfdc/config/model/dto/User.java
@@ -1,13 +1,25 @@
 package com.etl.sfdc.config.model.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class User {
+    @NotNull
     private Integer id;
+
+    @NotNull
     private String name;
+
+    @NotNull
     private String username;
+
+    @NotNull
     private String password;
+
+    @NotNull
     private String email;
+
+    @NotNull
     private String description;
 }
